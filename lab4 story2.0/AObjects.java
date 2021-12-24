52a3c40cadc4fc99e60bc89851357ae43bb9e2b8
inksrgv <94178896+inksrgv@users.noreply.github.com>
2021-12-24 10:50:18 +0300

    Delete lab4 story2.0 directory

--- lab4 story2.0/AObjects.java
+++
@@ -1,8 +0,0 @@
-public abstract class AObjects implements IObjects {
-    protected String name;
-
-    public String getName() {
-        return this.name = name;
-    }
-
-}
--- lab4 story2.0/IObjects.java
+++
@@ -1,3 +0,0 @@
-public interface IObjects {
-    String getName();
-}
